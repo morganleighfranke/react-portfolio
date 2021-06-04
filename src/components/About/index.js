5f34d1da0947d3770f8a7d6843cac4403a06e447
Morgan Franke <morganleighfranke@gmail.com>
2021-06-04 09:58:12 -0400

    navbar and about components up and running

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+import Resume from './Resume/MorganFrankeTechnicalResume.pdf';
+
+function About() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default About;
