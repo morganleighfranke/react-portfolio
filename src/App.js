b2d0fa25d69dd9d47ff238a783748a505fa6bf83
Morgan Franke <morganleighfranke@gmail.com>
2021-06-05 20:00:36 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/react-portfolio" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/contact" component={Contact} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
           </Switch>
           <Footer />
     </div>
